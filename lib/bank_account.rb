6c96f29edccd4dbcc3ae6db463b863416704facc
github username <github email address>
2018-09-20 18:58:50 -0400

    completed Banck Account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
-class BankAccount
+require 'pry'
 
-end
+
+  class BankAccount #allows us to reade the balance
+
+    attr_reader :name
+    attr_accessor :deposit, :status, :balance
+
+    def initialize(name)
+      @name = name
+      # @deposit = deposit
+      @balance = 1000
+      @status = "open"
+
+    end
+
+    def deposit(deposit)
+      # self.balance + amount
+      @balance += deposit
+    end
+
+    def display_balance
+      "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+      if @status == "open" && @balance > 0
+        return true
+      else
+        return false
+      end
+    end
+
+    def close_account
+      self.status = "closed"
+    end
+
+  end
